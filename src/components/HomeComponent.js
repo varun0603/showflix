4e5dc13b79be34bc31afaddb8f87fe0829c8491b
Keshav Reddy <keshavlingala@gmail.com>
2020-05-21 14:26:06 +0530

    bootstrap added

--- src/components/HomeComponent.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const HomeComponent = ({movies}) => {
-  return <div></div>;
-};
-
-export default HomeComponent;
---
+++ src/components/HomeComponent.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { LinearProgress, Card, CardActionArea, CardMedia, CardContent, Typography, CardActions, Button, makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+});
+const HomeComponent = ({ movies }) => {
+  const classes = useStyles();
+  if (movies.errMess) {
+    return <h1>{movies.errMess}</h1>
+  }
+  if (movies.isLoading) {
+    return (
+      <div className="mt-4">
+        <LinearProgress />
+      </div>
+    )
+  }
+  console.log(movies.movies.data.movies);
+  const moviesview = movies.movies.data.movies.map(movie => {
+    return (
+      <div key={movie.id} className="m-4">
+        <Card className={classes.root}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              alt="Contemplative Reptile"
+              height="140"
+              image={movie.medium_cover_image}
+              title="Contemplative Reptile"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                {movie.title}
+          </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                {movie.description_full}
+          </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary">
+              Share
+        </Button>
+            <Button size="small" color="primary">
+              Learn More
+        </Button>
+          </CardActions>
+        </Card>
+      </div>
+    );
+  })
+  return moviesview;
+};
+
+export default HomeComponent;
