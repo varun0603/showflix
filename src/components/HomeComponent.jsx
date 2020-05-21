763636802d36244fb15cec28ddcd9960edcfb6a3
Keshav Reddy <keshavlingala@gmail.com>
2020-05-21 15:00:53 +0530

    UI: Movie Cards Added 2 per row

--- src/components/HomeComponent.jsx
+++ src/components/HomeComponent.jsx
@@ -1,68 +1,75 @@
 import React from "react";
-import { 
-  LinearProgress,
-  Card, 
-  CardActionArea, 
-  CardMedia, 
-  CardContent, 
-  Typography, 
-  CardActions, 
-  Button, 
-  makeStyles 
+import {
+    LinearProgress,
+    Card,
+    CardActionArea,
+    CardMedia,
+    CardContent,
+    Typography,
+    CardActions,
+    Button,
+    makeStyles
 } from '@material-ui/core'
 
 const useStyles = makeStyles({
-  root: {
-    maxWidth: 345,
-  },
+    root: {
+        maxWidth: 345,
+    },
 });
-const HomeComponent = ({ movies }) => {
-  const classes = useStyles();
-  if (movies.errMess) {
-    return <h1>{movies.errMess}</h1>
-  }
-  if (movies.isLoading) {
+const HomeComponent = ({movies}) => {
+    const classes = useStyles();
+    if (movies.errMess) {
+        return <h1>{movies.errMess}</h1>
+    }
+    if (movies.isLoading) {
+        return (
+            <div className="mt-4">
+                <LinearProgress/>
+            </div>
+        )
+    }
+    console.log(movies.movies.data.movies);
+    const moviesview = movies.movies.data.movies.map(movie => {
+        return (
+            <div key={movie.id} className="col-sm-4">
+                <div className="m-4">
+                    <Card className={classes.root}>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                alt="Contemplative Reptile"
+                                image={movie.medium_cover_image}
+                                title="Contemplative Reptile"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                    {movie.title}
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    {movie.description_full}
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                            <Button size="small" color="primary">
+                                Share
+                            </Button>
+                            <Button size="small" color="primary">
+                                Learn More
+                            </Button>
+                        </CardActions>
+                    </Card>
+                </div>
+            </div>
+        );
+    });
     return (
-      <div className="mt-4">
-        <LinearProgress />
-      </div>
-    )
-  }
-  console.log(movies.movies.data.movies);
-  const moviesview = movies.movies.data.movies.map(movie => {
-    return (
-      <div key={movie.id} className="m-4">
-        <Card className={classes.root}>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              alt="Contemplative Reptile"
-              height="140"
-              image={movie.medium_cover_image}
-              title="Contemplative Reptile"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-                {movie.title}
-          </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-                {movie.description_full}
-          </Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions>
-            <Button size="small" color="primary">
-              Share
-        </Button>
-            <Button size="small" color="primary">
-              Learn More
-        </Button>
-          </CardActions>
-        </Card>
-      </div>
+        <div className="container">
+            <div className="row">
+                {moviesview}
+            </div>
+        </div>
     );
-  })
-  return moviesview;
 };
 
 export default HomeComponent;
